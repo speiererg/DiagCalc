528d38b39bdd866139fd56b76c11c22340ef26db
speiererg <64892214+speiererg@users.noreply.github.com>
2022-10-16 23:42:46 +0200

    lkj

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 var modifier_nbr = 3
 var modifierTyp_nbr = 4
-var array_inputs = [];
+var array_inputs_value = [];
+var array_inputs_itemNbr = []
 
 // Loading 
 function loadingIndex(){
@@ -15,13 +16,15 @@ function document_addeventlistener(){
 function click_calculate(){
 for (let i=1;i<=modifier_nbr;i++){
     let array_inputs_oneModifier = [];
+    let item_nbr = 0
 for (let k=1;k<=modifierTyp_nbr;k++){
     let input_value = document.getElementById(`input${i}_${k}`).value
     if (document.getElementById(`input${i}_${k}`).value!=''){
+        item_nbr++
         array_inputs_oneModifier.push(input_value)
-
     }
 }
+    array_inputs_itemNbr.push(item_nbr)
     array_inputs.push(array_inputs_oneModifier);
 
 }
