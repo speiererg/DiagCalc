853e3ef249179fa851b7b4eac30e0ca4cb4465bb
speiererg <64892214+speiererg@users.noreply.github.com>
2022-10-17 07:33:31 +0200

    sdf

--- js/index.js
+++ js/index.js
@@ -1,36 +1,43 @@
 var modifier_nbr = 3
 var modifierTyp_nbr = 4
 var array_inputs_value = [];
-var array_inputs_itemNbr = []
+var array_inputs_itemNbr = [];
+var array_calculator = [];
 
 // Loading 
-function loadingIndex(){
+function loadingIndex() {
     document_addeventlistener()
-console.log('test')
+    console.log('test')
 }
 
-function document_addeventlistener(){
-    document.getElementById('button_calculate').addEventListener('click',click_calculate)
+function document_addeventlistener() {
+    document.getElementById('button_calculate').addEventListener('click', click_calculate)
 }
 
-function click_calculate(){
-for (let i=1;i<=modifier_nbr;i++){
-    let array_inputs_oneModifier = [];
-    let item_nbr = 0
-for (let k=1;k<=modifierTyp_nbr;k++){
-    let input_value = document.getElementById(`input${i}_${k}`).value
-    if (document.getElementById(`input${i}_${k}`).value!=''){
-        item_nbr++
-        array_inputs_oneModifier.push(input_value)
+function click_calculate() {
+    for (let i = 1; i <= modifier_nbr; i++) {
+        let array_inputs_oneModifier = [];
+        let item_nbr = 0
+        for (let k = 1; k <= modifierTyp_nbr; k++) {
+            let input_value = document.getElementById(`input${i}_${k}`).value
+            if (document.getElementById(`input${i}_${k}`).value != '') {
+                item_nbr++
+                array_inputs_oneModifier.push(input_value)
+            }
+        }
+        array_inputs_itemNbr.push(item_nbr)
+        array_inputs_value.push(array_inputs_oneModifier);
     }
-}
-    array_inputs_itemNbr.push(item_nbr)
-    array_inputs_value.push(array_inputs_oneModifier);
 
+    // Creation du array_Calculator
+array_calculator.unshift(1) 
+for (let i = modifier_nbr; i>0;i--){
+array_calculator.unshift(array_calculator[0]*array_inputs_itemNbr[i-1])
 }
-console.log(array_inputs_value)
-console.log(array_inputs_itemNbr)
 
+    console.log(array_inputs_value)
+    console.log(array_inputs_itemNbr)
+console.log(array_calculator)
 }
 
 
