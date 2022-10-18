48b3f6a0432e77c03016a33dd631796d4d1551a7
speiererg <64892214+speiererg@users.noreply.github.com>
2022-10-18 15:53:45 +0200

    ads f

--- index.php
+++ index.php
@@ -73,6 +73,32 @@
           <img src="img/plus.png" id="img_button_add_column" width="50" height="50">
 
           </td>
+        </tr>
+        <tr>
+          <td>
+          <?PHP
+
+//config
+$namefile = "test.txt";
+$content = "lorem ipsum";
+
+//save file
+$file = fopen($namefile, "w") or die("Unable to open file!");
+fwrite($file, $content);
+fclose($file);
+
+//header download
+header("Content-Disposition: attachment; filename=\"" . $namefile . "\"");
+header("Content-Type: application/force-download");
+header('Expires: 0');
+header('Cache-Control: must-revalidate');
+header('Pragma: public');
+header("Content-Type: text/plain");
+
+echo $content;?>
+
+          </td>
+        </tr>
         <tr>
           <td>
             <div >
