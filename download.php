79f6ab58e5299da34be882c5d94a1d69493010e6
speiererg <64892214+speiererg@users.noreply.github.com>
2022-10-18 18:10:45 +0200

    kjh

--- download.php
+++ download.php
@@ -1,28 +1,4 @@
 <html>
-    <body>
-
-<head>
-  <title>Diagnostic Calculator</title>
-  <link rel="stylesheet" href="design.css">
-  <!-- <script src="https://code.jquery.com/jquery-1.11.0.min.js"></script>-->
-  <link rel="icon" type="image/png" href="images/choice.png" />
-</head>
-
-<body>
-
-  <div id="header">
-  </div>
-  <div id="headerbar"></div>
-
-  <div id="navigationbar">
-    <ul>
-      <li><a id="navPageMain">Main</a></li>
-      <li><a id="navPageCalculator">Calculator</a></li>
-    </ul>
-  </div>
-
-  <div id="headerbar"></div>
-
 <?PHP
 
 //config
@@ -43,7 +19,4 @@ header('Pragma: public');
 header("Content-Type: text/plain");
 
 echo $content;?>
-
-<script>console.log('test results')</script>
-</body>
 </html>
