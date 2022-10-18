187ba7b432a809bc67fe845e7e8aaedae9d97617
speiererg <64892214+speiererg@users.noreply.github.com>
2022-10-18 16:00:07 +0200

    asdf

--- download.php
+++ download.php
@@ -1,6 +1,28 @@
 <html>
     <body>
 
+    <head>
+  <title>Diagnostic Calculator</title>
+  <link rel="stylesheet" href="design.css">
+  <!-- <script src="https://code.jquery.com/jquery-1.11.0.min.js"></script>-->
+  <link rel="icon" type="image/png" href="images/choice.png" />
+</head>
+
+<body>
+
+  <div id="header">
+  </div>
+  <div id="headerbar"></div>
+
+  <div id="navigationbar">
+    <ul>
+      <li><a id="navPageMain">Main</a></li>
+      <li><a id="navPageCalculator">Calculator</a></li>
+    </ul>
+  </div>
+
+  <div id="headerbar"></div>
+
 <?PHP
 
 //config
