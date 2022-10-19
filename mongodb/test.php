7ab894e9a72cf4b2e8fa32f4ac6a32a049f7a2f4
speiererg <64892214+speiererg@users.noreply.github.com>
2022-10-19 13:46:06 +0200

    adf

--- mongodb/test.php
+++ mongodb/test.php
@@ -1,5 +1,5 @@
 <?php
-  require 'vendor/autoload.php';
+  require '../vendor/autoload.php';
 $client = new MongoDB\Client('mongodb+srv://speiererg:guichsp2004Pi@cluster0.lhafb.mongodb.net/?retryWrites=true&w=majority');
 $db = $client->test;
 
