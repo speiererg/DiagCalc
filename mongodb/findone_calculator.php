c885896967042e89e77fc9bc7fcf99081c3a4bf2
speiererg <64892214+speiererg@users.noreply.github.com>
2022-10-19 21:01:13 +0200

    asdf

--- mongodb/findone_calculator.php
+++ mongodb/findone_calculator.php
@@ -1,18 +1,7 @@
 <?php
   require '../vendor/autoload.php';
 
-$inputs_output = array();
-
-echo  $_POST['modifierSub_nbr'];
-
-for ($i = 1; $i <= $_POST['modifier_nbr']; $i++) {
-   for ($k = 1; $k <= $_POST['modifierSub_nbr']; $k++) {
-      $input_ID = "input{$i}_{$k}";
-      $inputs_output = array_merge($inputs_output, array("input{$i}_{$k}" => $_POST[$input_ID]));
-   }
-}
-
-  $client = new MongoDB\Client('mongodb+srv://speiererg:guichsp2004Pi@cluster0.lhafb.mongodb.net/?retryWrites=true&w=majority');
+$client = new MongoDB\Client('mongodb+srv://speiererg:guichsp2004Pi@cluster0.lhafb.mongodb.net/?retryWrites=true&w=majority');
 $db = $client->test;
 
 
