3eed37c969ce2db65fcd4139ea72f833870e396f
speiererg <64892214+speiererg@users.noreply.github.com>
2022-10-19 14:59:27 +0200

    asdf

--- mongodb/add_calculator.php
+++ mongodb/add_calculator.php
@@ -3,7 +3,7 @@
 
 $inputs_output = array();
 
-echo  $_POST['modifier_nbr'];
+echo  $_POST['modifierSub_nbr'];
 
 for ($i = 1; $i <= $_POST['modifier_nbr']; $i++) {
    for ($k = 1; $k <= $_POST['modifierSub_nbr']; $k++) {
@@ -11,7 +11,6 @@ for ($i = 1; $i <= $_POST['modifier_nbr']; $i++) {
       $inputs_output = array_merge($inputs_output, array("input{$i}_{$k}" => $_POST[$input_ID]));
    }
 }
-echo $inputs_output;
 
   $client = new MongoDB\Client('mongodb+srv://speiererg:guichsp2004Pi@cluster0.lhafb.mongodb.net/?retryWrites=true&w=majority');
 $db = $client->test;
