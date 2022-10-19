dcc84f048189a7cf26c87ee9e8fc2737af5adf3b
speiererg <64892214+speiererg@users.noreply.github.com>
2022-10-19 23:30:43 +0200

    asdf

--- mongodb/find_home.php
+++ mongodb/find_home.php
@@ -11,7 +11,7 @@ $collection = $client->DiagCalc_Calculators->Index;
 $item = $_POST['item'];
 $value = $_POST['value'];
 
-$params = array('Type' => 'Fruit');
+$params = array('active' => 'yes');
 
 $cursor = $collection->find($params);
 foreach ($cursor as $doc) {
