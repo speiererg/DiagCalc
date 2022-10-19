4609a8de795a4866ea627d7d330d45a40cbbfb68
speiererg <64892214+speiererg@users.noreply.github.com>
2022-10-19 21:16:35 +0200

    asdf

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ function findOne_Calculator_mongoDB(id,version){
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             myObj = JSON.parse(this.responseText);
-           console.log(myObj)
+           console.log(myObj.input)
         }
     };
     xmlhttp.open("GET", "mongodb/findone_calculator.php", true);
