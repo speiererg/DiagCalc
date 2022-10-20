2e27d390fbad4564642a0e6aa9cd67574fa449d3
speiererg <64892214+speiererg@users.noreply.github.com>
2022-10-20 10:12:17 +0200

    sdfg

--- app.js
+++ app.js
@@ -107,6 +107,21 @@ function find_home_mongoDB(id, version) {
 }
 
 
+function findOne_Calculator_mongoDB(id, version) {
+    console.log('test')
+    var xmlhttp = new XMLHttpRequest();
+    let params = `id=${id}&version=${version}`
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            myObj = JSON.parse(this.responseText);
+            console.log(myObj)
+            console.log(myObj['inputs'])
+        }
+    };
+    xmlhttp.open("POST", "mongodb/findOne_calculator.php", true);
+    xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xmlhttp.send(params);
+}
 
 
 
