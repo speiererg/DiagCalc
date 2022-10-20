850f48409c5a71a962e91a2d4b94c40d182cf016
speiererg <64892214+speiererg@users.noreply.github.com>
2022-10-20 07:37:38 +0200

    adf

--- mainContent/home.php
+++ mainContent/home.php
@@ -1,7 +1,11 @@
 <h1>Index</h1>
 <table id ="home_table">
 <tr id="home_tr">
-<td class="home_td">
+<td class="home_td" id="home_td1">
+<td class="home_td"id="home_td2">
+<td class="home_td"id="home_td3">
+<td class="home_td"id="home_td4">
+
 </td>
 </tr>
 </table>
