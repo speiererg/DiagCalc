e931c39f06771e6b1832ba4b062ada75f984e08c
speiererg <64892214+speiererg@users.noreply.github.com>
2022-10-20 08:35:14 +0200

    adsf

--- calculator.js
+++ calculator.js
@@ -4,6 +4,7 @@ document.getElementById('input_hidden_modifierSub_nbr').value = modifierSub_nbr
 
 function loading_page_calculator() {
     document_addeventlistener_calculator()
+    console.log('test')
     console.log(id_POST, version_POST)
     findOne_Calculator_mongoDB(id_POST, version_POST)
 }
