e390114ba0e4cf60f5f61e06d11814310d99a5ec
speiererg <64892214+speiererg@users.noreply.github.com>
2022-10-21 14:45:56 +0200

    asdf

--- mainContent/calculator.php
+++ mainContent/calculator.php
@@ -10,7 +10,7 @@ echo "<input type='hidden' name='input_hidden_POST_version' id='input_hidden_POS
           <td>
            Main Diagnose: <input type="text" id="input_maindiagnose" name="input_maindiagnose" class="input_maindiagnose" value="" form="form_saveMongoDB"> 
            Version: <select id="select_version" name="select_version" class="input_select" form="form_saveMongoDB"><option value="1" selected>Version 1</option></select>
-           Calculator_id: <input type="number" id="input_calculator_id" name="calculator_id" class="input_maindiagnose" value="" form="form_saveMongoDB"> 
+           Calculator_id: <input type="number" id="input_calculator_id" name="calculator_id" class="input_calculator_id" value="" form="form_saveMongoDB"> 
            EDG_id: <input type="number" id="input_EDG_id" name="EDG_id" class="input_maindiagnose" value="" form="form_saveMongoDB" readonly> 
         
           </td>
