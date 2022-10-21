665a491a80b756a1fdd5223f41c007ecd118e255
speiererg <64892214+speiererg@users.noreply.github.com>
2022-10-21 13:36:10 +0200

    asdf

--- app.js
+++ app.js
@@ -114,6 +114,7 @@ function findOne_Calculator_mongoDB(id, version) {
     xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             let results_findOne = JSON.parse(this.responseText);
+            console.log(reults_findOne)
             importing_calculator(results_findOne)
         }
     };
