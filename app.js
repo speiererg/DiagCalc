4f5585ed434b71d44a2681d3e40afaf065a5f1ee
speiererg <64892214+speiererg@users.noreply.github.com>
2022-10-21 13:53:59 +0200

    adsf

--- app.js
+++ app.js
@@ -131,6 +131,7 @@ function importing_calculator(results_findOne) {
     for (let l=0;l<modifier_nbr_target;l++){addInputColumn();console.log('inputColumn')}
     modifierSub_nbr_target = results_findOne['modifierSub_nbr']
     for (let i=0;i<modifierSub_nbr_target;i++){addInputRow()}
+    for( i=0;i<inputs.length;i++){document.getElementById(inputs[i][0]).value = inputs[i][1]}
 
 }
 
