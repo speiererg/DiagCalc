aa966b07c4d4cb422130fe289da166f032b8725e
speiererg <64892214+speiererg@users.noreply.github.com>
2022-10-21 21:09:14 +0200

    asdf

--- mongodb/find_home.php
+++ mongodb/find_home.php
@@ -1,5 +1,5 @@
 <?php
- // require '../vendor/autoload.php';
+require '../vendor/autoload.php';
 $client = new MongoDB\Client('mongodb+srv://speiererg:guichsp2004Pi@cluster0.lhafb.mongodb.net/?retryWrites=true&w=majority');
 
 $collection = $client->DiagCalc_Calculators->Index;
