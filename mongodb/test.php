9342dcdd13baf66c350a9cf8d20dff439c014b59
speiererg <64892214+speiererg@users.noreply.github.com>
2022-10-22 21:10:34 +0200

    asdf

--- mongodb/test.php
+++ mongodb/test.php
@@ -9,7 +9,7 @@ $collection = $client->DiagCalc_Calculators->Calculators;
 // $version = $_POST['version'];
 
 $cursor = $collection->find(
-    ['calculator_id'=>'1']
+    array('calculator_id'=>'1')
 );
 /*
 $cursor->sort(
