e1fbe2fe71b3a7091f5cce9d7917aae7c2575f5d
speiererg <64892214+speiererg@users.noreply.github.com>
2022-10-22 13:21:57 +0200

    kjh

--- app.js
+++ app.js
@@ -199,7 +199,7 @@ function click_radio_input(event) {
 }
 
 function click_calculate() {
-    document.getElementById(`radio_input_2`).checked = true
+    document.getElementById(`radio_input_3`).checked = true
     array_inputs_value = [];
     array_inputs_itemNbr = [];
     array_calculator = [];
