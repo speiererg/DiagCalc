a37590dc690f51419520e6d881e01e718b186816
speiererg <64892214+speiererg@users.noreply.github.com>
2022-10-23 16:17:55 +0200

    asdf

--- mongodb/findOne_calculator.php
+++ mongodb/findOne_calculator.php
@@ -6,7 +6,7 @@ $client = new MongoDB\Client('mongodb+srv://speiererg:guichsp2004Pi@cluster0.lha
 $collection = $client->DiagCalc_Calculators->Calculators;
 $id = intval($_POST['id']);
 $version = intval($_POST['version']);
-$cursor = $collection->findOne(array('calculator_id' => $id, 'version' => $version"));
+$cursor = $collection->findOne(array('calculator_id' => $id, 'version' => $version));
 
 // 
 
