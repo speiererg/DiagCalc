2f8bc7bbce2f8c3d542b2a06ab7a837f3455f54b
speiererg <64892214+speiererg@users.noreply.github.com>
2022-10-23 07:33:53 +0200

    adf

--- mongodb/test.php
+++ mongodb/test.php
@@ -27,7 +27,7 @@ $cursor = $collection->find(
 //echo print_r($cursor);
 //echo iterator_to_array($cursor);
 $test = json_encode(iterator_to_array($cursor));
-echo $test[0]; 
+echo $test[1]; 
 //echo json_encode(iterator_to_array($cursor));
 ?>
 
