f2ee1c49eef3f7c294326ebaf68983ab0cb26902
speiererg <64892214+speiererg@users.noreply.github.com>
2022-10-23 16:48:50 +0200

    asdf

--- mainContent/calculator.php
+++ mainContent/calculator.php
@@ -12,9 +12,8 @@ echo "<input type='hidden' name='input_hidden_POST_version' id='input_hidden_POS
             <input type="hidden" name="calculator_id" id="input_hidden_form_deactivate_calculator" value="" >
             <input type="submit" name="button2"  class="button_download" value="Soft Delete" />
               </form> 
-              <form id="form_deactivage_calculator" action="" method="POST">
-            <input type="submit" name=""  class="button_download" value="Edit" />
-              </form>   
+            <input type="button" name=""  class="button_download" value="Edit" />
+            
 </td>
 </tr>
         <tr id="tr_maindiagnose">
