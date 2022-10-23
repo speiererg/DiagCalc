14347511e4642a5e7fae7eb046c8f3f897803fc0
speiererg <64892214+speiererg@users.noreply.github.com>
2022-10-23 16:50:37 +0200

    asdf

--- mainContent/calculator.php
+++ mainContent/calculator.php
@@ -7,13 +7,11 @@ echo "<input type='hidden' name='input_hidden_POST_version' id='input_hidden_POS
 
 <table class="CSS_contentTable">
   <tr>
-    <td width="100px">
+    <td>
     <form id="form_deactivage_calculator" action="mongodb/deactivateOne_calculator.php" method="POST">
             <input type="hidden" name="calculator_id" id="input_hidden_form_deactivate_calculator" value="" >
             <input type="submit" name="button2"  class="button_download" value="Soft Delete" />
               </form>
-</td>
-              <td> 
             <input type="button" name=""  class="button_download" value="Edit" />
             
 </td>
