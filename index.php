2106bc7bb8ec3e0ddf5de0d7ac2baa3e0cdff2c1
speiererg <64892214+speiererg@users.noreply.github.com>
2022-10-25 14:20:28 +0200

    adsf

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ session_start();
 <body>
 
   <div id="header">
-    <span   style="vertical-align:middle;">DiagCalc by <img src="img/logo.png" width="100px" style="margin:0px;padding:0px"/></span>
+    <span   style="vertical-align:middle;">DiagCalc by </span><img src="img/logo.png" width="100px" style="margin:0px;padding:0px"/>
   </div>
   <div id="headerbar"></div>
 
