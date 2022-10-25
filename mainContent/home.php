0633a8b2c8cc6f4ca19ebf13d8baadd6a0eeb5ac
speiererg <64892214+speiererg@users.noreply.github.com>
2022-10-25 09:12:11 +0200

    asdf

--- mainContent/home.php
+++ mainContent/home.php
@@ -7,8 +7,7 @@
 <td class="home_td"id="home_td2">
 <ul>
     </ul>
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
-  <link rel="stylesheet" href="/resources/demos/style.css">
+
   <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
   <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
   <script>
