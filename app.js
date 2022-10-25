4e5b3191d4e4fa8fd0a3f05858ea6354e9a347d4
speiererg <64892214+speiererg@users.noreply.github.com>
2022-10-25 17:20:46 +0200

    asdf

--- app.js
+++ app.js
@@ -217,7 +217,8 @@ function testMongoDB() {
         // The xml tag name.
         let XML_list = xml.getElementsByTagName('diag');
         console.log(XML_list)
-        console.log(XML_list[10][0])
+        console.log(XML_list[10])
+        console.log(XML_list[10][1])
     }
 
 
