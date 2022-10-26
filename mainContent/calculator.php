514382278603d201697b9c1b77db59eb7cba6e65
speiererg <64892214+speiererg@users.noreply.github.com>
2022-10-26 21:41:54 +0200

    adsf

--- mainContent/calculator.php
+++ mainContent/calculator.php
@@ -99,8 +99,8 @@ echo "<input type='hidden' name='input_hidden_POST_version' id='input_hidden_POS
         <tr>
           <td>
             <div >
-              <h3 id="total_count" style="display:none;">Total Count: </h3>
               <table id="table_output_calculator">
+              <h3 id="total_count" style="display:none;">Total Count: </h3>
               </table>
             </div>
           </td>
