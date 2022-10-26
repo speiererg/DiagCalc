6137d0d087a63cdfccf24c28a9287005ba6054ff
speiererg <64892214+speiererg@users.noreply.github.com>
2022-10-26 21:04:24 +0200

    adsf

--- mainContent/calculator.php
+++ mainContent/calculator.php
@@ -82,7 +82,7 @@ echo "<input type='hidden' name='input_hidden_POST_version' id='input_hidden_POS
               <input type="hidden" name="modifierSub_nbr" id="input_hidden_modifierSub_nbr" value="" >
               <input type="hidden" name="XML_output" id="input_hidden_XML_output" value="" >
               <input type="hidden" name="TXT_output" id="input_hidden_TXT_output" value="" >
-                <input type="submit" name="button2" id="button_save_calculator"  class="button_download" value="Save in MongoDB" />
+                <input type="submit" name="button2" id="button_save_calculator"  class="button_download" value="Save new Version" />
               </form>  
             </td>
 </tr>
