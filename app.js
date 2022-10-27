db64903259f60b94602f085e144fb49b1ec7f7b8
speiererg <64892214+speiererg@users.noreply.github.com>
2022-10-27 20:41:01 +0200

    adsf

--- app.js
+++ app.js
@@ -601,19 +601,20 @@ function click_calculate() {
         if (document.getElementById(`checkbox_multiple_input_${i}`).checked == true && document.getElementById(`radio_input_${i}`).checked == false) {
 
             for (let k = 1; k <= modifierSub_nbr; k++) {
+                let input_value = document.getElementById(`input${i}_${k}`).value
+                let SNOMED_value = document.getElementById(`inputSNOMED${i}_${k}`).value
+                let ICD_value = document.getElementById(`inputICD${i}_${k}`).value.split('::')[0]
+
                 if (input_value != '') {
                     array_inputs_oneModifier.push('')
                     array_inputs_SNOMED_oneModifier.push('')
                     array_inputs_ICD_oneModifier.push('')
                     array_inputs_itemNbr.push(2)
                     console.log(`input${i}_${k}`)
-                    let input_value = document.getElementById(`input${i}_${k}`).value
-                    let SNOMED_value = document.getElementById(`inputSNOMED${i}_${k}`).value
-                    let ICD_value = document.getElementById(`inputICD${i}_${k}`).value.split('::')[0]
-                        array_inputs_value.push(array_inputs_oneModifier);
-                        array_SNOMED_value.push(array_inputs_SNOMED_oneModifier);
-                        array_ICD_value.push(array_inputs_ICD_oneModifier);
-                    
+                    array_inputs_value.push(array_inputs_oneModifier);
+                    array_SNOMED_value.push(array_inputs_SNOMED_oneModifier);
+                    array_ICD_value.push(array_inputs_ICD_oneModifier);
+
                 }
                 console.log(array_inputs_value)
             }
