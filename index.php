a92040cdb5c3557be5261fb6c4f79abb101991dd
speiererg <64892214+speiererg@users.noreply.github.com>
2022-10-31 20:01:50 +0100

    adsf

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 
 <?php
+/*
 session_start();
 // If the user is not logged in redirect to the login page...
 if (!isset($_SESSION['loggedin'])) {
   header('Location: login/login.html');
   exit;
 }
+*/
 ?>
 
 
