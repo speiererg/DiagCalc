9323142e3eb99f10755fbba0d764f36148e7b574
speiererg <64892214+speiererg@users.noreply.github.com>
2022-10-31 19:39:15 +0100

    sfg

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 
 <?php
+/*
 session_start();
 // If the user is not logged in redirect to the login page...
 if (!isset($_SESSION['loggedin'])) {
   header('Location: login/login.html');
   exit;
 }
+*/
 ?>
+
 
 <!DOCTYPE html>
 <html id="allHtml">
