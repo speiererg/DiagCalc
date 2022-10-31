0aa2da0d03d23d6d3cc629b34fc39e15e08b101b
speiererg <64892214+speiererg@users.noreply.github.com>
2022-11-01 00:16:40 +0100

    adf

--- app.js
+++ app.js
@@ -716,7 +716,7 @@ function calculating_calculator_output(array_inputs_value, array_SNOMED_value, a
         let calculated_ICD = ""
         let calculated_modifier = []
         for (let i0 = 0; i0 < array_calculator_inputs_modifierNbr; i0++) {
-            let input_value_loop = `${array_inputs_value[i0][array_iterate[i0]]} `
+            let input_value_loop = `${array_inputs_value[i0][array_iterate[i0]]}`
             let SNOMED_value_loop = `${array_SNOMED_value[i0][array_iterate[i0]]}`
             let ICD_value_loop = `${array_ICD_value[i0][array_iterate[i0]]}`
             let input_modifier_loop = `${array_inputs_value[i0][array_iterate[i0]]}`
