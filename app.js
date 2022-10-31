fa29b1a05465a7e05afbbac6e751b591039d4bc5
speiererg <64892214+speiererg@users.noreply.github.com>
2022-10-31 18:45:37 +0100

    adsf

--- app.js
+++ app.js
@@ -689,6 +689,7 @@ function click_calculate() {
 
 
 function calculating_calculator_output(array_inputs_value, array_SNOMED_value, array_ICD_value, array_inputs_modifierNbr) {
+    console.log(array_inputs_value)
     let output_array = []
     array_calculator_inputs_modifierNbr = array_inputs_modifierNbr.length  //give the number of array (modifier_nbr + multiple)
     document.getElementById('table_output_calculator').innerHTML = "";
