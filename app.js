df25bd291885b0200b7047ac4d8aa4419c1605bb
speiererg <64892214+speiererg@users.noreply.github.com>
2022-11-01 20:49:20 +0100

    adsf

--- app.js
+++ app.js
@@ -273,7 +273,7 @@ function importing_calculator(results_findOne) {
     document.getElementById('select_version').addEventListener('change', (event) => { changePage(`calculator`, `calculator_id=${results_findOne['calculator_id']}&version=${document.getElementById('select_version').value}`, '') })
     document.getElementById('select_version').innerHTML = ""
 
-    console.log(results_findOne['array_output'])
+    console.log(results_findOne['output_array'])
 
     // Create version
     for (let i = 1; i <= results_findOne['lastVersion']; i++) {
