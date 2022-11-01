5bcbfc2c6215c4a3f81c8e42f39ead791a4160ba
speiererg <64892214+speiererg@users.noreply.github.com>
2022-11-01 22:07:56 +0100

    asdf

--- app.js
+++ app.js
@@ -286,6 +286,7 @@ function loading_page_calculator(paramsJS) {
 }
 
 function importing_calculator(results_findOne) {
+    console.log(results_findOne)
     document.getElementById('button_edit_calculator').addEventListener('click', () => { edit_calculator() })
     document.getElementById('input_maindiagnose').value = results_findOne['mainName']
     document.getElementById('input_calculator_id').value = results_findOne['calculator_id']
