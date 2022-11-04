c5ab4186d5c24ec8f23ff8c9eeead5ec0804e03d
speiererg <64892214+speiererg@users.noreply.github.com>
2022-11-04 07:29:57 +0100

    adf

--- app.js
+++ app.js
@@ -97,6 +97,7 @@ function changePageExecute(page, paramsPHP, paramsJS) {
 
                 // if New Calculator
                 if (paramsJS == "newCalculator") {
+                    console.log('new Calculator')
                     document.getElementById('input_hidden_new_calculator').value = 1
                     document.getElementById('button_edit_calculator').disabled = false
                     confirmBeforeNavigate = 1
