f380326c294575bbc95533a0155f4c54654b0b6e
speiererg <64892214+speiererg@users.noreply.github.com>
2022-11-06 10:02:58 +0100

    asd

--- js/mapping.js
+++ js/mapping.js
@@ -6,9 +6,9 @@ function loading_page_mapping(){
 
 
 
-function mapping_SNOMED_search_onClick(){
+async function mapping_SNOMED_search_onClick(){
 let input_concept = document.getElementById('input_concept_SNOMED_search').value
-let output_SNOMED_Search = find_SNOMED_concept_mongoDB(input_concept)
+let output_SNOMED_Search = await find_SNOMED_concept_mongoDB(input_concept)
 console.log(output_SNOMED_Search)
 console.log(output_SNOMED_Search[0])
 
@@ -17,7 +17,7 @@ document.getElementById(mapping_ul_SNOMED).appendChild(mapping_create_li(`Id ${e
 });
 }
 
-function find_SNOMED_concept_mongoDB(concept) {
+async function find_SNOMED_concept_mongoDB(concept) {
     var xmlhttp = new XMLHttpRequest();
     let params = `concept=${concept}`;
     xmlhttp.onreadystatechange = function () {
@@ -31,7 +31,6 @@ function find_SNOMED_concept_mongoDB(concept) {
     xmlhttp.open("POST", "mongodb/concept_SNOMED_search.php", true);
     xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     xmlhttp.send(params);
-    return results_JSON
 }
 
 function mapping_diagnosis_search_onClick(){
