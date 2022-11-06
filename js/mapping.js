d322bb228b82c09c85d684d8240679630ed91c76
speiererg <64892214+speiererg@users.noreply.github.com>
2022-11-06 18:22:00 +0100

    adsf

--- js/mapping.js
+++ js/mapping.js
@@ -36,10 +36,28 @@ function find_SNOMED_concept_mongoDB(concept) {
 
 function mapping_SNOMED_remplace_onClick() {
     let input_concept = document.getElementById('input_concept_SNOMED_search').value
-    find_SNOMED_concept_mongoDB(input_concept)
+    remplace_SNOMED_concept_mongoDB(input_concept)
 }
 
-
+function remplace_SNOMED_concept_mongoDB(concept) {
+    var xmlhttp = new XMLHttpRequest();
+    let params = `concept=${concept}`;
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            //console.log(this.responseText)
+            let results_JSON = JSON.parse(this.responseText);
+            results_JSON.forEach(element => {
+                document.getElementById('mapping_ul_SNOMED').appendChild(mapping_create_li(
+                    `Id ${element['calculator_id']}: ${element['mainName']}`,
+                    `index.php?calculator=${element['calculator_id']}&version=${element['version']}`
+                ))
+            });
+        }
+    };
+    xmlhttp.open("POST", "mongodb/concept_SNOMED_search.php", true);
+    xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xmlhttp.send(params);
+}
 /* ********************************** Diagnostic ***************************************/
 
 
