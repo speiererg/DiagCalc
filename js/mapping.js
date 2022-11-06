88787b14bcb31ff29357dbbfea6e86b2f88e56d5
speiererg <64892214+speiererg@users.noreply.github.com>
2022-11-06 20:51:37 +0100

    asdf

--- js/mapping.js
+++ js/mapping.js
@@ -22,6 +22,7 @@ function find_SNOMED_concept_mongoDB(concept) {
             //console.log(this.responseText)
             let results_JSON = JSON.parse(this.responseText);
             results_JSON.forEach(element => {
+                document.getElementById('mapping_ul_SNOMED').innerHTML=""
                 document.getElementById('mapping_ul_SNOMED').appendChild(mapping_create_li(
                     `Id ${element['calculator_id']}: ${element['mainName']}`,
                     `index.php?calculator=${element['calculator_id']}&version=${element['version']}`
