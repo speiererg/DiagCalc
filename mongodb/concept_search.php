617869c348560bcb3d082ea272871a328b303583
speiererg <64892214+speiererg@users.noreply.github.com>
2022-11-06 08:46:58 +0100

    adsf

--- mongodb/concept_search.php
+++ mongodb/concept_search.php
@@ -17,7 +17,7 @@ $sPOSTConcept = $_POST['concept'];
 
 $cursor = $collection->aggregate(
   [
-    ['$match' => ['$expr' => ['$in'=> [123]]]],
+    ['$match' => ['modifiers..SNOMED_array' => ['$in'=> [123]]]],
     //[ '$group' => ['_id' => ['day' => '$day'], 'n' => ['$sum' => 1]  ] ],
     //[ '$sort' => ['_id' => 1] ],
     //[ '$limit' => 14 ]
