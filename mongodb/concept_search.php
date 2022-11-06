5fe0359bed77499b2a2d6d37bec5699aabfc2de0
speiererg <64892214+speiererg@users.noreply.github.com>
2022-11-06 08:12:42 +0100

    asdf

--- mongodb/concept_search.php
+++ mongodb/concept_search.php
@@ -16,7 +16,7 @@ $collection = $client->DiagCalc_Calculators->Calculators;
 $sPOSTConcept = $_POST['concept'];
 
 $cursor = $collection->find(
-  array('$in' , '1667573138' )
+  array('modifiers.0.SNOMED_array',array('$in' , '123' ))
 );
 
 /*
