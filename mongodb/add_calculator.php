5789f41c2e775de623791ab60581360164941a25
speiererg <64892214+speiererg@users.noreply.github.com>
2022-11-08 19:54:36 +0100

    sdf

--- mongodb/add_calculator.php
+++ mongodb/add_calculator.php
@@ -242,7 +242,7 @@ echo 'MedSP_term:' . json_encode($results_medSP_term) . '</br>';
 $medSP_array = json_decode(json_encode($results_medSP_term,true), true);
 
 echo 'MedSP Array:' . json_encode($medSP_array) . '</br>';
-echo 'search:' . gettype($medSP_array) . array_search("A2 B1",$medSP_array,false). '</br>';
+echo 'search:' . gettype($medSP_array) . array_search("A2 B1",$medSP_array[0],false). '</br>';
 echo 'test' . $medSP_array[0]['80000100063']. '</br>';
 }
 $cursor_lastEDGId = $collection_Index->findOne(
