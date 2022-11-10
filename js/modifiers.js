82b796d68fadc5dd84bdb5e031c8c3c0006aa4c8
speiererg <64892214+speiererg@users.noreply.github.com>
2022-11-10 20:05:45 +0100

    sdf

--- js/modifiers.js
+++ js/modifiers.js
@@ -1,5 +1,5 @@
 function loading_page_modifiers() {
-find_modifiers()
+    find_modifiers()
 }
 
 function find_modifiers() {
@@ -11,7 +11,8 @@ function find_modifiers() {
             console.log(results_JSON)
 
             document.getElementById('modifier_typ_ul').innerHTML = ""
-
+            let modifier_array_toDownload = []
+            let subModifier_array_toDownload = []
             for (let i = 0; i < Object.keys(results_JSON).length; i++) {
                 let element = results_JSON[i]
                 let link = document.createElement('li')
@@ -20,20 +21,24 @@ function find_modifiers() {
                 let text_name = document.createTextNode(`${element['modifier_name']} (id: ${element['modifier_id']}, DiagCalc: ${element['calculator_name']}[Version:${element['calculator_version']}])`)
                 let params_link = `calculator_id=${element['calculator_id']}&version=${element['calculator_version']}`
                 link.appendChild(text_name)
-//Create Sub                
+                modifier_array_toDownload.push([element['modifier_id'], element['modifier_name']])
+                //Create Sub                
                 let sub_ul = document.createElement('ul')
-                element['modifier_array'].forEach((element)=>{
+                element['modifier_array'].forEach((element) => {
                     let sub_li = document.createElement('li')
                     sub_li.appendChild(document.createTextNode(element))
                     sub_li.setAttribute('class', 'home_sublink')
                     sub_ul.appendChild(sub_li)
+                    subModifier_array_toDownload.push([element])
+
                 })
                 link.appendChild(sub_ul)
                 document.getElementById('modifier_typ_ul').appendChild(link)
                 console.log(params_link)
                 document.getElementById(`modifier_link_id_${element['modifier_id']}`).addEventListener('click', function () { changePage(`calculator`, params_link, '') })
             }
-
+            console.log(modifier_array_toDownload)
+            console.log(subModifier_array_toDownload)
         }
     };
     xmlhttp.open("POST", "mongodb/find_modifiers.php", true);
