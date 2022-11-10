bc84d02c50f6573b8ff5e7b24f460c417318c044
speiererg <64892214+speiererg@users.noreply.github.com>
2022-11-10 07:35:32 +0100

    kjh

---
+++ mainContent/modifier.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+// If the user is not logged in redirect to the login page...
+if (!isset($_SESSION['loggedin'])) {
+    header('Location: login/login.html');
+    exit;
+}
+?>
+<p>
+<h1 style="text-align:center;">Modifier Managment</h1>
+<h2>Modifier list</h2> 
+<p>
+<ul id="modifier_typ_ul_SNOMED">
+</ul>
