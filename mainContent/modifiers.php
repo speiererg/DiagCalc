1600d396019ae39888d54bcca1fadd59547e235b
speiererg <64892214+speiererg@users.noreply.github.com>
2022-11-10 20:15:07 +0100

    sdf

--- mainContent/modifiers.php
+++ mainContent/modifiers.php
@@ -19,7 +19,7 @@ if (!isset($_SESSION['loggedin'])) {
             <form name="form" action="download/download_modifier_XML.php" method="POST">
               <input type="hidden" name="input_XML" id="input_XML" value="">
               <input type="button" class="button_download" id="button_download_modifier_XML"value="Download as XML" />
-              <input type="submit" name="button1" class="button_download" id="button_download_modifier_XML_submit" value="Download as XML" style="display:none"/>
+              <input type="submit" name="button1" class="button_download" id="button_download_modifier_XML_submit" value="Download Modifier as XML" style="display:none"/>
             </form>
             <p>
 
@@ -27,14 +27,14 @@ if (!isset($_SESSION['loggedin'])) {
             <form name="form" action="download/download_modifier_TXT.php" method="POST">
               <input type="hidden" name="input_TXT" id="input_TXT" value="">
               <input type="button" class="button_download" id="button_download_modifier_TXT" value="Download as TXT" />
-              <input type="submit" name="button2" class="button_download" id="button_download_modifier_TXT_submit" value="Download as TXT" style="display:none"/>
+              <input type="submit" name="button2" class="button_download" id="button_download_modifier_TXT_submit" value="Download Modifier as TXT" style="display:none"/>
             </form>
           </td>
           <td>
             <form name="form" action="download/download_sub_modifier_XML.php" method="POST">
               <input type="hidden" name="input_XML" id="input_XML" value="">
               <input type="button" class="button_download" id="button_download_sub_modifier_XML"value="Download as XML" />
-              <input type="submit" name="button1" class="button_download" id="button_download_sub_modifier_XML_submit" value="Download as XML" style="display:none"/>
+              <input type="submit" name="button1" class="button_download" id="button_download_sub_modifier_XML_submit" value="Download Sub-Modifier as XML" style="display:none"/>
             </form>
             <p>
 
@@ -42,7 +42,7 @@ if (!isset($_SESSION['loggedin'])) {
             <form name="form" action="download/download_sub_modifier_TXT.php" method="POST">
               <input type="hidden" name="input_TXT" id="input_TXT" value="">
               <input type="button" class="button_download" id="button_download_sub_modifier_TXT" value="Download as TXT" />
-              <input type="submit" name="button2" class="button_download" id="button_download__sub_modifier_TXT_submit" value="Download as TXT" style="display:none"/>
+              <input type="submit" name="button2" class="button_download" id="button_download__sub_modifier_TXT_submit" value="Download Sub-Modifier as TXT" style="display:none"/>
             </form>
           </td>
         </tr>
