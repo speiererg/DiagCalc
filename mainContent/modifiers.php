606a1f6185c25ade05b7c5b30186433ff8a53aa1
speiererg <64892214+speiererg@users.noreply.github.com>
2022-11-10 20:14:22 +0100

    sdf

--- mainContent/modifiers.php
+++ mainContent/modifiers.php
@@ -12,3 +12,38 @@ if (!isset($_SESSION['loggedin'])) {
 <p>
 <ul id="modifier_typ_ul">
 </ul>
+<table>
+        <tr>
+
+          <td>
+            <form name="form" action="download/download_modifier_XML.php" method="POST">
+              <input type="hidden" name="input_XML" id="input_XML" value="">
+              <input type="button" class="button_download" id="button_download_modifier_XML"value="Download as XML" />
+              <input type="submit" name="button1" class="button_download" id="button_download_modifier_XML_submit" value="Download as XML" style="display:none"/>
+            </form>
+            <p>
+
+
+            <form name="form" action="download/download_modifier_TXT.php" method="POST">
+              <input type="hidden" name="input_TXT" id="input_TXT" value="">
+              <input type="button" class="button_download" id="button_download_modifier_TXT" value="Download as TXT" />
+              <input type="submit" name="button2" class="button_download" id="button_download_modifier_TXT_submit" value="Download as TXT" style="display:none"/>
+            </form>
+          </td>
+          <td>
+            <form name="form" action="download/download_sub_modifier_XML.php" method="POST">
+              <input type="hidden" name="input_XML" id="input_XML" value="">
+              <input type="button" class="button_download" id="button_download_sub_modifier_XML"value="Download as XML" />
+              <input type="submit" name="button1" class="button_download" id="button_download_sub_modifier_XML_submit" value="Download as XML" style="display:none"/>
+            </form>
+            <p>
+
+
+            <form name="form" action="download/download_sub_modifier_TXT.php" method="POST">
+              <input type="hidden" name="input_TXT" id="input_TXT" value="">
+              <input type="button" class="button_download" id="button_download_sub_modifier_TXT" value="Download as TXT" />
+              <input type="submit" name="button2" class="button_download" id="button_download__sub_modifier_TXT_submit" value="Download as TXT" style="display:none"/>
+            </form>
+          </td>
+        </tr>
+      </table>
