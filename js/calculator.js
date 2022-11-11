2b2c9742f175038fb308ab35eb91db9eab423748
speiererg <64892214+speiererg@users.noreply.github.com>
2022-11-11 22:57:17 +0100

    sdf

--- js/calculator.js
+++ js/calculator.js
@@ -645,6 +645,8 @@ function calculating_calculator_output(array_inputs_value, array_SNOMED_value, a
     document.getElementById('button_edit_calculator').disabled = false
     document.getElementById('button_calculate').disabled = true
     document.getElementById('button_save_calculator').disabled = false
+    document.getElementById('button_save_review').disabled = true
+
     console.log(output_array)
     disable_input(true)
     printing_calculator_output(output_array, 'calculate')
