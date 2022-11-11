fe7e326cd7a616f197c5e231422351505fc915ca
speiererg <64892214+speiererg@users.noreply.github.com>
2022-11-11 23:19:21 +0100

    sdf

--- mongodb/find_home.php
+++ mongodb/find_home.php
@@ -38,7 +38,7 @@ $cursor = $collection->find(
   );
 }else if ($_POST['request'] == "to_be_reviewed_coding"){
   $cursor = $collection->find(
-    ['review_coding.checked' => "false"],
+    ['reviewed_coding.checked' => "false"],
     ['sort' => array('mainName' => 1)]
   );
 }
