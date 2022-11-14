e099e5acb8888a0c03f54df78970b54bb789239e
speiererg <64892214+speiererg@users.noreply.github.com>
2022-11-14 21:06:23 +0100

    df

--- js/modifiers.js
+++ js/modifiers.js
@@ -18,7 +18,7 @@ function find_modifiers() {
             for (let i = 0; i < Object.keys(results_JSON).length; i++) {
                 let element = results_JSON[i]
                 let link = document.createElement('div')
-                link.setAttribute('class', 'home_link item_flex')
+                link.setAttribute('class', 'item_flex')
                 link.setAttribute('id', `modifier_link_id_${element['modifier_id']}`)
                 let text_name = document.createTextNode(`${element['modifier_name']} (id: ${element['modifier_id']}, DiagCalc: ${element['calculator_name']}[Version:${element['calculator_version']}])`)
                 let params_link = `calculator_id=${element['calculator_id']}&version=${element['calculator_version']}`
