02de70b96bad7075bd6f396c6e0350bbe2c2724d
speiererg <64892214+speiererg@users.noreply.github.com>
2022-11-15 22:04:06 +0100

    df

---
+++ download/download_diagCalc_ready_import_XML.php
@@ -0,0 +1,20 @@
+<?PHP
+
+//config
+$namefile = "EPIC_import_CSV.xml";
+$content = $_POST['input_hidden_diag_calc_XML'];
+
+//save file
+$file = fopen($namefile, "w") or die("Unable to open file!");
+fwrite($file, $content);
+fclose($file);
+
+//header download
+header("Content-Disposition: attachment; filename=\"" . $namefile . "\"");
+header("Content-Type: application/force-download");
+header('Expires: 0');
+header('Cache-Control: must-revalidate');
+header('Pragma: public');
+header("Content-Type: text/plain");
+
+echo $content;?>
--- mainContent/importing.php
+++ mainContent/importing.php
@@ -14,39 +14,9 @@ if (!isset($_SESSION['loggedin'])) {
 <ul id="importing_diagnostic_ul">
 </ul>
 <p>
-    
-<table>
-        <tr>
-
-          <td>
-            <form name="form" action="download/download_modifier_XML.php" method="POST">
-              <input type="hidden" name="input_hidden_modifier_XML" id="input_hidden_modifier_XML" value="">
-              <input type="button" class="button_download" id="button_download_modifier_XML"value="Download Modifier as XML" />
-              <input type="submit" name="button1" class="button_download" id="button_download_modifier_XML_submit" style="display:none"/>
+            <form name="form" action="download/download_diagCalc_ready_import_XML.php" method="POST">
+              <input type="hidden" name="input_hidden_modifier_XML" id="input_hidden_diag_calc_XML" value="input_hidden_diag_calc_XML">
+              <input type="button" class="button_download" id="button_download_diagCalc_ready_import_XML"value="Download all DiagCalc ready to import XML" />
+              <input type="submit" name="button1" class="button_download" id="button_download__diagCalc_ready_import_XML_submit" style="display:none"/>
             </form>
             <p>
-
-
-            <form name="form" action="download/download_modifier_TXT.php" method="POST">
-              <input type="hidden" name="input_hidden_modifier_TXT" id="input_hidden_modifier_TXT" value="">
-              <input type="button" class="button_download" id="button_download_modifier_TXT" value="Download Modifier as TXT" />
-              <input type="submit" name="button2" class="button_download" id="button_download_modifier_TXT_submit" style="display:none"/>
-            </form>
-          </td>
-          <td>
-            <form name="form" action="download/download_sub_modifier_XML.php" method="POST">
-              <input type="hidden" name="input_hidden_sub_modifier_XML" id="input_hidden_sub_modifier_XML" value="">
-              <input type="button" class="button_download" id="button_download_sub_modifier_XML"value="Download Sub-Modifier as XML" />
-              <input type="submit" name="button1" class="button_download" id="button_download_sub_modifier_XML_submit" style="display:none"/>
-            </form>
-            <p>
-
-
-            <form name="form" action="download/download_sub_modifier_TXT.php" method="POST">
-              <input type="hidden" name="input_hidden_sub_modifier_TXT" id="input_hidden_sub_modifier_TXT" value="">
-              <input type="button" class="button_download" id="button_download_sub_modifier_TXT" value="Download Sub-Modifier as TXT" />
-              <input type="submit" name="button2" class="button_download" id="button_download__sub_modifier_TXT_submit" style="display:none"/>
-            </form>
-          </td>
-        </tr>
-      </table>
